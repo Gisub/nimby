1387279d6b8fc429dfc5a147b2a0fec0fcf7a0d0
Gisub <105473642+Gisub@users.noreply.github.com>
2022-05-21 11:57:28 +0900

    Update tractor_nimby_swich.sh

--- shell/tractor_nimby_swich.sh
+++ shell/tractor_nimby_swich.sh
@@ -4,9 +4,9 @@ switch=$1
 host_name=$2
 ip_name=$3
 
-root_id=`/usr/bin/curl "http://192.168.10.11/Tractor/monitor?q=login&user=root"|jq -r .tsid`
+root_id=`/usr/bin/curl "http://123.456.78.09/Tractor/monitor?q=login&user=root"|jq -r .tsid`
 
-echo "http://192.168.10.11/Tractor/ctrl?q=battribute&tsid=${root_id}&b=${host_name}/${ip_name}&nimby=${switch}"
-curl "http://192.168.10.11/Tractor/ctrl?q=battribute&tsid=${root_id}&b=${host_name}/${ip_name}&nimby=${switch}"
+echo "http://123.456.78.09/Tractor/ctrl?q=battribute&tsid=${root_id}&b=${host_name}/${ip_name}&nimby=${switch}"
+curl "http://123.456.78.09/Tractor/ctrl?q=battribute&tsid=${root_id}&b=${host_name}/${ip_name}&nimby=${switch}"
 echo "nimby Okay"
 
