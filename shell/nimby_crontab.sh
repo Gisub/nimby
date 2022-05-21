6cc224a31501346365c8765777fb0e6cb8c2067c
Gisub <105473642+Gisub@users.noreply.github.com>
2022-05-21 11:56:43 +0900

    Delete nimby_crontab.sh

--- shell/nimby_crontab.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PYTHONPATH=/core/TD/PPlib:/core/Linux/Lib/PythonLib:/core/TD/sg4th/src:/core/TD/shotgunAPI/python-api-master:$PYTHONPATH
-export PATH=/core/Linux/APPZ/Python/2.7/bin:$PATH
-export LD_LIBRARY_PATH=/core/Linux/APPZ/Python/2.7/lib:$LD_LIBRARY_PATH
-
-/core/Linux/APPZ/Python/2.7/bin/python2.7 /core/Linux/APPZ/shell/nimby/nimby_checker.py
